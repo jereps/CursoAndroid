13e59aa96c08ba0b8a84c2c59cc01d1abcc9e83b
Jeremias Ponciano da Silva <jeremiason2911@gmail.com>
2016-08-30 14:09:15 -0300

    Correção de bug no projeto Login. Não reconhecia classe R

--- Login/app/src/main/java/br/com/login/MainActivity.java
+++ Login/app/src/main/java/br/com/login/MainActivity.java
@@ -2,6 +2,7 @@ package br.com.login;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
