607197bd249ebcc774eef5d9ce0b03563fab8c4c
Jeremias Ponciano <jeremiason2911@gmail.com>
2016-10-15 11:24:22 -0300

    Feito classe DbController para contralar leitura e escrita do DB

--- ProjProfessor/Sqlite/app/src/main/java/br/com/sqlite/datacontroller/DbController.java
+++ ProjProfessor/Sqlite/app/src/main/java/br/com/sqlite/datacontroller/DbController.java
@@ -1,6 +1,8 @@
 package br.com.sqlite.datacontroller;
 
 
+import android.content.ContentValues;
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
 import br.com.sqlite.database.DbHelper;
@@ -12,6 +14,46 @@ public class DbController {
     private static final String leitura = "leitura";
     private static final String escrita = "escrita";
 
-    
+    public DbController(Context context){
+        dbHelper = new DbHelper(context);
+    }
+
+    //abre a conexão com o DB
+    private static SQLiteDatabase openConn(String operacao){
+        if (db == null || !db.isOpen())
+
+            if (operacao.equals(escrita))
+                db = dbHelper.getWritableDatabase();
+            else
+                db = dbHelper.getReadableDatabase();
+
+        return db;
+    }
+
+    //fechar conexão com o DB
+    private static void closseConn(){
+
+        if (dbHelper != null) dbHelper.close();
+        if(db != null) db.close();
+    }
+
+    //isert
+    public String insertRegistro(String email, String senha){
+
+        long resultado;
+
+        openConn(escrita);
+        ContentValues valores = new ContentValues();
+        valores.put("email_usuario", email);
+        valores.put("senha_usuario", senha);
+        resultado = db.insertWithOnConflict("usuarios", null, valores, SQLiteDatabase.CONFLICT_REPLACE);
+        closseConn();
+
+        if (resultado == -1)
+            return "Erro ao inserir registro";
+        else
+            return "Registro inserido com sucesso";
+
+    }
 
 }
