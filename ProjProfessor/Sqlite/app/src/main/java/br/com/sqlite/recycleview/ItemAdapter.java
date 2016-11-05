d04790afb5ecf898198b4a342e576b9d115bae21
Jeremias Ponciano da Silva <jeremiason2911@gmail.com>
2016-11-05 10:11:08 -0300

    Criado package recycleview, e class ItemAdapter, uma classe abstrata, que extende a class RecyclerView.Adapter<ItemAdapter.ViewHolder>, e implementado seus métodos.

--- ProjProfessor/Sqlite/app/src/main/java/br/com/sqlite/recycleview/ItemAdapter.java
+++ ProjProfessor/Sqlite/app/src/main/java/br/com/sqlite/recycleview/ItemAdapter.java
@@ -1,5 +1,23 @@
 package br.com.sqlite.recycleview;
 
 
-public class ItemAdapter {
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
+
+    @Override
+    public ItemAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ItemAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
