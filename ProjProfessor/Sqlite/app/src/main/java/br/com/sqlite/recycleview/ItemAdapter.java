677cc1165502de887bbcbce554a69f9fefd88737
Jeremias Ponciano da Silva <jeremiason2911@gmail.com>
2016-11-05 10:34:35 -0300

    alterado a ViewHolder dentro da ItemAdapter para poder manipular a class R.

--- ProjProfessor/Sqlite/app/src/main/java/br/com/sqlite/recycleview/ItemAdapter.java
+++ ProjProfessor/Sqlite/app/src/main/java/br/com/sqlite/recycleview/ItemAdapter.java
@@ -1,15 +1,35 @@
 package br.com.sqlite.recycleview;
 
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.List;
+
+import br.com.sqlite.R;
+import br.com.sqlite.datamodel.ItemLogin;
 
 public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
 
+    private Context context;
+    private List<ItemLogin> lista;
+
+    //contrutor
+    public ItemAdapter (Context context, List<ItemLogin> lista){
+        this.context = context;
+        this.lista = lista;
+    }
+
     @Override
-    public ItemAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View rootView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_user, parent, false);
+        return new ViewHolder(rootView);
     }
 
     @Override
@@ -22,8 +42,12 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
 
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
+        TextView email, senha;
+        ImageButton btndelete;
+
         public ViewHolder(View itemView) {
             super(itemView);
+            email = (TextView)itemView.findViewById(R.id.)
         }
 
         @Override
