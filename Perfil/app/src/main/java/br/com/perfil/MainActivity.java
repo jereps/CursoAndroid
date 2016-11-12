0b34602267c2e8e7d2c54eec72c8b9df30942c50
Jeremias Ponciano Da Silva <jeremiason2911@gmail.com>
2016-11-12 11:48:33 -0300

    trabalho final

--- Perfil/app/src/main/java/br/com/perfil/MainActivity.java
+++ Perfil/app/src/main/java/br/com/perfil/MainActivity.java
@@ -1,13 +1,57 @@
 package br.com.perfil;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnPerfil;
+    private EditText edtNomeCompleto;
+    private EditText edtProfissao;
+    private Spinner spnRenda;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnPerfil = (Button) findViewById(R.id.btn_perfil);
+        edtNomeCompleto = (EditText) findViewById(R.id.edt_nome_completo);
+        edtProfissao = (EditText) findViewById(R.id.edt_profissao);
+        spnRenda = (Spinner) findViewById(R.id.spn_renda);
+
+        btnPerfil.setOnClickListener(new View.OnClickListener(){
+
+
+
+
+            @Override
+            public void onClick(View view) {
+
+                String nome = edtNomeCompleto.getText().toString();
+                String profissao = edtProfissao.getText().toString();
+                int posicao = (int) spnRenda.getSelectedItemId();
+
+                if (nome.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Informe seu nome", Toast.LENGTH_SHORT).show();
+                }
+                else if (posicao == 0) {
+                    Toast.makeText(MainActivity.this, "Informe sua renda", Toast.LENGTH_SHORT).show();
+                }
+                else if (profissao.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Informe sua profissão", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    startActivity(new Intent(MainActivity.this, PerfilActivity.class));
+                }
+
+            }
+        });
     }
 }
--- Perfil/app/src/main/java/br/com/perfil/PerfilActivity.java
+++ Perfil/app/src/main/java/br/com/perfil/PerfilActivity.java
@@ -1,13 +1,27 @@
 package br.com.perfil;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PerfilActivity extends AppCompatActivity {
+
+    private Button btncontato;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
+
+        btncontato = (Button) findViewById(R.id.btn_contato);
+
+        btncontato.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(PerfilActivity.this, ContatoActivity.class));
+            }
+        });
     }
 }
